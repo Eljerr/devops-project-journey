b4b6e133bb97663529217953d98135fd78955786
eljerFromWsl <eliazerraditya@gmail.com>
2026-06-12 17:42:48 +0700

    feat: add end point for healt and readiness

--- src-app/app.js
+++ src-app/app.js
@@ -8,4 +8,26 @@ app.get('/', (req, res) => {
     db_host: process.env.DB_HOST || "Tidak ada koneksi DB"
   });
 });
+
+app.get('/healtz', (req,res) => {
+  res.status(200).send('OK')
+})
+
+app.get('/ready', async (req,res) => {
+  try {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'secret',
+      database: 'mysql'
+    })
+      await connection.query('SELECT 1')
+      console.log('✅ Database connection successful!')
+      
+      await connection.end()
+  } catch (error) {
+    console.error('Database connection failed')
+  }
+})
+
 module.exports = app
